8bed2dd58aff4351077eed9260a0333ea663e257
Wes Hatley <whatley@lenovo.com>
2021-10-25 16:22:16 -0700

    polish

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import express from 'express';
 import fetcher from './fetchApi.js';
 import axios from 'axios';
+import { Sequelize } from 'sequelize';
 
 const app = express();
 const port = 42069;
 
 app.get('/', (req, res) => {
   fetcher();
+  res.send('send');
 });
 
 app.listen(port, () => {
-  axios.get(`http://localhost:${port}`)
+  axios.get(`http://localhost:${port}`);
   console.log(`Chill app listening at http://localhost:${port}, nice.`);
 });
